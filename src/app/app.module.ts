44de5c0df606882b9cf08d1f560d6f6d8bbe5981
liyanyu <liyanyu@cn.cogenda.com>
2018-01-18 13:30:01 +0800

    update angular and typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -172,6 +173,7 @@ import { ShortdetailComponent } from './shortdetail/shortdetail.component'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     HttpModule,
